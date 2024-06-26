4290d1d371d8e16984874c454467222682769ddc
Supriya Mahajan <mahajansupriya77@gmail.com>
2024-06-26 23:26:04 +0530

    Setting up the project

---
+++ Rest API using Executor Framework/Rest-API-using-Executor-Framework/src/test/java/com/bnt/RestApiUsingExecutorFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiUsingExecutorFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
